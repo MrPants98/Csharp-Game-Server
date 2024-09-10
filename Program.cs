6bdd903f31960a65a12b39098f701f2a07fc063a
ocecil <ocecil98@gmail.com>
2024-09-09 20:36:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Unity_Game_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
